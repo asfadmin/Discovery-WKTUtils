48f88a5df41f017c8d3e5afefbd9bc9d8c0f5bd5
Cameron Showalter <cjshowalter@alaska.edu>
2020-07-14 22:56:09 -0800

    Switched setup from ssh, to https

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A few WKT utilities for use elsewhere",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="git@github.com/asfadmin/Discovery-WKTUtils.git",
+    url="https://github.com/asfadmin/Discovery-WKTUtils.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
