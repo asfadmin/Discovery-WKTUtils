64bfab48c2b01a4092cd3fcd1df98d31366a4eec
Cameron Showalter <cjshowalter@alaska.edu>
2021-03-02 21:56:37 -0900

    Switching to building off github tags

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
+import subprocess
+
+# Get the version from git and save it:
+wktutils_version = subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+assert "." in wktutils_version, "ERROR: Cannot get version from git. Version: '{0}'.".format(wktutils_version)
+
+# with open("WKTUTils/VERSION", "w+") as fh:
+#     fh.write(str(wktutils_version) + "\n")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="WKTUtils",
-    version="0.0.5",
+    version=wktutils_version,
     author="ASF Discovery Team",
     author_email="uaf-asf-discovery@alaska.edu",
     description="A few WKT utilities for use elsewhere",
@@ -13,6 +21,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/asfadmin/Discovery-WKTUtils.git",
     packages=setuptools.find_packages(),
+    # package_data= {'WKTUtils': ['VERSION']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
