475541889b441284f7c6de1f4e5076c347dd0105
Cameron Showalter <cjshowalter@alaska.edu>
2021-08-24 14:47:50 -0800

    Switching to new pytest-automation, instead of running from the repo

--- yml_tests/pytest_managers.py
+++ yml_tests/pytest_managers.py
@@ -3,12 +3,12 @@ from test_repairWKT import test_repairWKT_manager
 from test_filesToWKT import test_filesToWKT_manager
 from test_validateWKT import test_validateWKT_manager
 
-def test_repairWKT(test_info, file_conf, cli_args, test_vars):
-    test_repairWKT_manager(test_info, file_conf, cli_args, test_vars)
+def test_repairWKT(**args):
+    test_repairWKT_manager(**args)
 
-def test_filesToWKT(test_info, file_conf, cli_args, test_vars):
-    test_filesToWKT_manager(test_info, file_conf, cli_args, test_vars)
+def test_filesToWKT(**args):
+    test_filesToWKT_manager(**args)
 
-def test_validateWKT(test_info, file_conf, cli_args, test_vars):
-    test_validateWKT_manager(test_info, file_conf, cli_args, test_vars)
+def test_validateWKT(**args):
+    test_validateWKT_manager(**args)
 
