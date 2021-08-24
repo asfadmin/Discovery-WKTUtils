eeddd9638b5f95d275293611a74cb909916399bf
Cameron Showalter <cjshowalter@alaska.edu>
2021-08-24 15:06:17 -0800

    Moved the 'import WKtils' logic to the manager, so it doesn't need ot be duplicated for each test

--- yml_tests/pytest_managers.py
+++ yml_tests/pytest_managers.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Add the package to pythonpath before import:
+root_dir = os.path.abspath(os.path.join(__file__,"..",".."))
+if root_dir not in sys.path:
+    sys.path.append(root_dir)
 
 from test_repairWKT import test_repairWKT_manager
 from test_filesToWKT import test_filesToWKT_manager
